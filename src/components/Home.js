b5701f6b292f5837a085a6ba020974b37c79a6ec
Ashu Sharma <ashusharma7644@gmail.com>
2024-04-03 21:04:22 +0530

    new file

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,19 @@ const Home = () => {
     <>
       <div className='main-home'>
         <div className='inner-home'>
-          <div>
-            <h1>Home</h1>
+          <div className='banner-content'>
+            <h2>High Quality Test Station</h2>
+            <h1>We believe Good Food Offer Great Smile</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className='banner-btn'>
+            <button className='banner-btn-one'>Book Link Table</button>
+            <button className='banner-btn-two'>View More</button>
+            </div>
+            
           </div>
-          <div></div>
+          <div className='banner-media'>
+            <img src='./images/banner-img.png'/>
+          </div>
         </div>
       </div>
     </>
