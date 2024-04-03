4bc8e2a2dd0fc7d923519da08995d6e6a3ed8cf9
Ashu Sharma <ashusharma7644@gmail.com>
2024-04-03 18:59:41 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <div className='main-home'></div>
+      <div className='main-home'>
+        <div className='inner-home'>
+          <div>
+            <h1>Home</h1>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </>
   )
 }
